811f433c36cba43b07565d5aeb9e6f937addf993
Anupal Mishra <anupal.pm@gmail.com>
2022-04-19 00:01:40 +0530

    add flask app to microblog project

---
+++ project-1/app.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://anupalmishra:52xHQg8MSkK9@mircroblog-application.lpp36.mongodb.net/test")
+    app.db = client.mircroblog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            date = datetime.today()
+            formatted_date = date.strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+            print(entry_content, formatted_date)
+
+        get_entries = [(e["content"], e["date"], datetime.strptime(e["date"], "%Y-%m-%d").strftime("%b %d")) for e in app.db.entries.find({})]
+        print(get_entries)
+        return render_template("home.html", entries=get_entries)
+    return app
