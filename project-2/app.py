4270ab01885202c17fcb6630a1efb3b14cfbc424
Anupal Mishra <anupal.pm@gmail.com>
2022-04-22 00:56:07 +0530

    add logic to complete habit and habit css

--- project-2/app.py
+++ project-2/app.py
@@ -1,8 +1,10 @@
 import datetime
-from flask import Flask, render_template, request
+from collections import defaultdict
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-habits = ["Test Habit", "Test Habit 2"]
+habits = ["Test Habit"]
+completions = defaultdict(list)
 
 @app.context_processor
 def add_calc_date_range():
@@ -22,6 +24,7 @@ def index():
         "index.html",
         habits=habits,
         title="Habit Tracker - Home",
+        completions=completions[selected_date],
         selected_date=selected_date)
 
 @app.route("/add", methods=["GET", "POST"])
@@ -33,3 +36,14 @@ def add_habit():
         "add_habit.html",
         title="Habbit Tracker - Add Habit",
         selected_date=datetime.date.today())
+
+
+@app.route("/complete", methods=["GET", "POST"])
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habitName")
+    date = datetime.date.fromisoformat(date_string)
+    completions[date].append(habit)
+    print("hello hello")
+
+    return redirect(url_for("index", date=date_string))
